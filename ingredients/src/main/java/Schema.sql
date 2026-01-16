abb882edf29717ccd247d9630bd1b3268e1b5adf
Sangy-kun <rakotofif6@gmail.com>
2026-01-16 15:12:30 +0300

    suite TD

---
+++ ingredients/src/main/java/Schema.sql
@@ -0,0 +1,72 @@
+CREATE DATABASE mini_dish_db;
+
+CREATE USER mini_dish_db_manager WITH PASSWORD 'ton_mot_de_passe';
+
+GRANT ALL PRIVILEGES ON DATABASE mini_dish_db TO mini_dish_db_manager;
+
+
+
+--creation des enum
+
+CREATE TYPE dish_type_enum AS ENUM ('START', 'MAIN', 'DESSERT');
+CREATE TYPE category_enum AS ENUM ('VEGETABLE', 'ANIMAL', 'MARINE', 'DAIRY', 'OTHER');
+
+CREATE TABLE dish (
+                      id SERIAL PRIMARY KEY,
+                      name VARCHAR(255) NOT NULL,
+                      dish_type dish_type_enum NOT NULL
+);
+
+CREATE TABLE ingredient (
+                            id SERIAL PRIMARY KEY,
+                            name VARCHAR(255) NOT NULL,
+                            price NUMERIC(10,2) NOT NULL,
+                            category category_enum NOT NULL,
+                            id_dish INT,
+                            CONSTRAINT fk_dish FOREIGN KEY (id_dish) REFERENCES dish(id)
+);
+
+INSERT INTO dish (id, name, dish_type) VALUES
+                                           (1, 'Salade fraîche', 'START'),
+                                           (2, 'Poulet grillé', 'MAIN'),
+                                           (3, 'Riz aux légumes', 'MAIN'),
+                                           (4, 'Gâteau au chocolat', 'DESSERT'),
+                                           (5, 'Salade de fruits', 'DESSERT');
+
+INSERT INTO ingredient (id, name, price, category, id_dish) VALUES
+                                                                (1, 'Laitue', 800.00, 'VEGETABLE', 1),
+                                                                (2, 'Tomate', 600.00, 'VEGETABLE', 1),
+                                                                (3, 'Poulet', 4500.00, 'ANIMAL', 2),
+                                                                (4, 'Chocolat', 3000.00, 'OTHER', 4),
+                                                                (5, 'Beurre', 2500.00, 'DAIRY', 4);
+
+--Suite sujet donc nouveau sql (je ne sais pas si je dois mettre à jour l'ancien ou non donc je vais
+--directement reecrire un nouveau
+
+
+-- Table Ingredient (ingrédients uniques)
+CREATE TABLE ingredient (
+                            id SERIAL PRIMARY KEY,
+                            name VARCHAR(255) NOT NULL UNIQUE,  --pour éviter doublons
+                            price NUMERIC(10,2) NOT NULL,
+                            category category_enum NOT NULL
+);
+
+
+CREATE TABLE dish_ingredient (
+                                 dish_id INT NOT NULL,
+                                 ingredient_id INT NOT NULL,
+                                 PRIMARY KEY (dish_id, ingredient_id),  -- pas de doublons
+                                 FOREIGN KEY (dish_id) REFERENCES dish(id) ON DELETE CASCADE,
+                                 FOREIGN KEY (ingredient_id) REFERENCES ingredient(id) ON DELETE CASCADE
+);
+
+/*ON DELETE CASCADE : c'est comme si je supprimais un plat du menu donc je supprime aussi le lien dans la table
+  de jonction afin que la base reste propre
+  C'est une contrainte sql pour les clés etrangère
+ */
+
+
+
+
+
